23cd6027a5cf0b3015c6a7c6d43952a18b78cafa
Milosz Jakubczak <milosz.jakubczak@brival.co>
2026-05-27 09:42:04 +0200

    fix: add clearMocks to ensure mock isolation between tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'jest-expo',
+  clearMocks: true,
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|react-native-.*|@react-native(-community)?|@react-native-async-storage/.*)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|wonka|@supabase)',
